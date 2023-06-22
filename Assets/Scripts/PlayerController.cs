f6a477302573346a560d53673d72dbd583bf7c14
BaksaZoo <baksazoo@hotmail.com>
2023-06-22 22:39:53 +0200

    feat: replace legacy input system with new input system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float acceleration;
+
+    [Header("Physics")]
+    [SerializeField] private float horizontalDrag;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private Vector2 groundCheckPosition;
+    [SerializeField] private LayerMask whatIsGround;
+
+    // self controlled variables
+    private bool isGrounded;
+    private float moveInput;
+
+    // references
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        GroundCheck();
+        ApplyMoveInput(moveInput);
+    }
+
+    void GroundCheck()
+    {
+        Collider2D groundCheckCollider = Physics2D.OverlapCircle(rb.position + groundCheckPosition, groundCheckRadius, whatIsGround);
+        if (groundCheckCollider != null)
+        {
+            if (rb.velocity.y < 0f)
+            {
+                isGrounded = true;
+                animator.SetBool("IsGrounded", isGrounded);
+            }
+        }
+        else
+        {
+            isGrounded = false;
+            animator.SetBool("IsGrounded", isGrounded);
+        }
+    }
+
+    void ApplyMoveInput(float movement)
+    {
+        movement *= acceleration;
+        float velocityToMaxSpeedRatio = Mathf.Abs(rb.velocity.x / maxSpeed);
+        float lerp = Mathf.Lerp(maxSpeed, 0f, velocityToMaxSpeedRatio);
+
+        Vector2 force = new Vector2(movement * lerp * acceleration * Time.fixedDeltaTime, 0f);
+        Vector2 drag = new Vector2(-rb.velocity.x * horizontalDrag * Time.fixedDeltaTime, 0f);
+
+        rb.AddForce(force + drag, ForceMode2D.Force);
+        //rb.AddForce(drag, ForceMode2D.Force);
+        // force - drag?
+        animator.SetFloat("Speed", movement);
+    }
+
+    public void Move(float moveInput) => this.moveInput = moveInput;
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            Vector2 vel = rb.velocity;
+            vel.y = jumpSpeed;
+            rb.velocity = vel;
+            animator.SetBool("IsGrounded", isGrounded);
+            animator.SetTrigger("Jump"); 
+        }
+    }
+
+    void OnDrawGizmosSelected() => Gizmos.DrawWireSphere((Vector2)transform.position + groundCheckPosition, groundCheckRadius);
+
+    void OnDisable() => animator.SetFloat("Speed", 0f);
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,111 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerController))]
 public class PlayerMovement : MonoBehaviour
 {
-	public float speed = 5f;
-	public float maxSpeed = 10f;
+    PlayerController controller;
 
-	public float jumpSpeed = 5f;
+    void Start() => controller = GetComponent<PlayerController>();
 
-	public float horizontalDrag = 2f;
+    public void OnHorizontalMovement(InputValue inputValue) => controller.Move(inputValue.Get<float>());
 
-	public Vector2 groundCheckPosition;
-	public float groundCheckRadius;
-	public LayerMask whatIsGround;
-
-	public string horizontalInput;
-	public string jumpInput;
-
-	public Animator animator;
-
-	Rigidbody2D rb;
-
-	private float movement;
-	private bool jump;
-
-	private bool isGrounded;
-
-	private bool isFlipped;
-
-    void Start()
+    public void OnJump(InputValue inputValue)
     {
-		rb = GetComponent<Rigidbody2D>();
+        if (inputValue.isPressed) controller.Jump();
     }
-
-	// Update is called once per frame
-	void Update()
-    {
-		movement = Input.GetAxisRaw(horizontalInput) * speed;
-		if (Input.GetButtonDown(jumpInput) && isGrounded)
-			jump = true;
-
-		animator.SetFloat("Speed", Mathf.Abs(movement));
-
-		Collider2D collider = Physics2D.OverlapCircle(rb.position + groundCheckPosition, groundCheckRadius, whatIsGround);
-		if (collider != null)
-		{
-			if (rb.velocity.y < 0f)
-			{
-				isGrounded = true;
-				animator.SetBool("IsGrounded", isGrounded);
-			}
-		} else
-		{
-			isGrounded = false;
-			animator.SetBool("IsGrounded", isGrounded);
-		}
-    }
-
-	private void FixedUpdate()
-	{
-		float t = rb.velocity.x / maxSpeed;
-
-		float lerp = 0f;
-
-		if (t >= 0f)
-			lerp = Mathf.Lerp(maxSpeed, 0f, t);
-		else if (t < 0f)
-			lerp = Mathf.Lerp(maxSpeed, 0f, Mathf.Abs(t));
-
-		Vector2 force = new Vector2(movement * lerp * speed * Time.fixedDeltaTime, 0f);
-		Vector2 drag = new Vector2(-rb.velocity.x * horizontalDrag * Time.fixedDeltaTime, 0f);
-
-		rb.AddForce(force, ForceMode2D.Force);
-		rb.AddForce(drag, ForceMode2D.Force);
-
-		if (movement >= .1f && isFlipped)
-		{
-			Vector2 flipScale = new Vector2(-1f, 1f);
-			animator.transform.localScale *= flipScale;
-			isFlipped = false;
-		} else if (movement <= -.1f && !isFlipped)
-		{
-			Vector2 flipScale = new Vector2(-1f, 1f);
-			animator.transform.localScale *= flipScale;
-			isFlipped = true;
-		}
-
-		if (jump)
-		{
-			Vector2 vel = rb.velocity;
-			vel.y = jumpSpeed;
-			rb.velocity = vel;
-			jump = false;
-			isGrounded = false;
-			animator.SetBool("IsGrounded", isGrounded);
-			animator.SetTrigger("Jump");
-		}
-	}
-
-	private void OnDrawGizmosSelected()
-	{
-		Gizmos.DrawWireSphere((Vector2)transform.position + groundCheckPosition, groundCheckRadius);
-	}
-
-	private void OnDisable()
-	{
-		animator.SetFloat("Speed", 0f);
-	}
 }
